cbd5d78bf323b5cd2e11c86ed61969dca0309293
Maciej Zygmunt <maciekzygmunt@interia.pl>
2022-07-18 18:41:45 +0200

    added chart

--- components/DailyChart.tsx
+++ components/DailyChart.tsx
@@ -9,9 +9,20 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import { useEffect, useRef } from 'react';
 import { Line } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
 
 export const DailyChart = ({ weather1d }: any) => {
   const minTemps = weather1d.map((day: any) => Math.round(day.values.minTemp * 2) / 2);
@@ -22,18 +33,26 @@ export const DailyChart = ({ weather1d }: any) => {
     const dayOfMonth = date.getDate();
     return dayOfMonth;
   });
+  const canva = useRef();
+
+  useEffect(() => {
+    canva?.current.resize(1024, 150);
+  }, []);
 
   const options = {
-    responsive: true,
+    responsive: false,
+    maintainAspectRatio: false,
     plugins: {
       legend: {
         display: false,
       },
+      ChartDataLabels,
+      datalabels: {
+        color: '#353535',
+        anchor: 'end',
+      },
     },
-    tension: 0.2,
     display: false,
-    // maintainAspectRatio: false,
-    width: '128px',
   };
 
   const data = {
@@ -54,6 +73,6 @@ export const DailyChart = ({ weather1d }: any) => {
     ],
   };
 
-  return <Line options={options} data={data} />;
+  return <Line ref={canva} options={options} data={data} />;
 };
 export default DailyChart;
