35b6f8e3b319465dd29d15ecc8fbb03d1494c77e
Maciej Zygmunt <maciekzygmunt@interia.pl>
2022-11-05 18:47:14 +0100

    changed icons to static on hourly

--- components/WeatherIcon.tsx
+++ components/WeatherIcon.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
+
 import Sun from '../assets/animated-icons/day.svg';
-import Moon from '../assets/animated-icons/night.svg';
-import MoonBig from '../assets/animated-icons/nightBig.svg';
+import Moon from '../assets/animated-icons/nightBig.svg';
 import SunClouds from '../assets/animated-icons/cloudy-day-2.svg';
 import MoonClouds from '../assets/animated-icons/cloudy-night-2.svg';
 import Clouds from '../assets/animated-icons/cloudy.svg';
@@ -12,6 +13,19 @@ import LightSnow from '../assets/animated-icons/snowy-4.svg';
 import Snow from '../assets/animated-icons/snowy-5.svg';
 import HeavySnow from '../assets/animated-icons/snowy-6.svg';
 import Thunder from '../assets/animated-icons/thunder.svg';
+
+import SunSmall from '../assets/static-icons/day.svg';
+import MoonSmall from '../assets/static-icons/night.svg';
+import SunCloudsSmall from '../assets/static-icons/cloudy-day-2.svg';
+import MoonCloudsSmall from '../assets/static-icons/cloudy-night-2.svg';
+import CloudsSmall from '../assets/static-icons/cloudy.svg';
+import LightRainSmall from '../assets/static-icons/rainy-4.svg';
+import RainSmall from '../assets/static-icons/rainy-5.svg';
+import HeavyRainSmall from '../assets/static-icons/rainy-6.svg';
+import LightSnowSmall from '../assets/static-icons/snowy-4.svg';
+import SnowSmall from '../assets/static-icons/snowy-5.svg';
+import HeavySnowSmall from '../assets/static-icons/snowy-6.svg';
+import ThunderSmall from '../assets/static-icons/thunder.svg';
 interface WeatherIconProps {
   code: number;
   time: number;
@@ -22,35 +36,69 @@ const WeatherIcon = ({ code, time, big = false }: WeatherIconProps) => {
   let icon = <></>;
   if (code === 1000) {
     if (time < 20 && time > 5) {
-      icon = <Sun className="overflow-visible" />;
-    } else if (big == false) {
-      icon = <Moon className="overflow-visible" />;
-    } else if (big == true) {
-      icon = <MoonBig className="overflow-visible w-44" />;
+      icon = big ? <Sun className="overflow-visible" /> : <SunSmall className="overflow-visible" />;
+    } else {
+      icon = big ? (
+        <Moon className="overflow-visible w-44" />
+      ) : (
+        <MoonSmall className="overflow-visible" />
+      );
     }
   } else if (code === 1100 || code === 1101) {
     if (time < 20 && time > 5) {
-      icon = <SunClouds className="overflow-visible" />;
+      icon = big ? (
+        <SunClouds className="overflow-visible" />
+      ) : (
+        <SunCloudsSmall className="overflow-visible" />
+      );
     } else {
-      icon = <MoonClouds className="overflow-visible" />;
+      icon = big ? (
+        <MoonClouds className="overflow-visible" />
+      ) : (
+        <MoonCloudsSmall className="overflow-visible" />
+      );
     }
   } else if (code === 1102 || code === 1001 || code === 2100 || code === 2000) {
-    icon = <Clouds className="overflow-visible" />;
+    icon = big ? (
+      <Clouds className="overflow-visible" />
+    ) : (
+      <CloudsSmall className="overflow-visible" />
+    );
   } else if (code === 4000 || code === 4200 || code === 6000 || code === 6200) {
-    icon = <LightRain className="overflow-visible" />;
+    icon = big ? (
+      <LightRain className="overflow-visible" />
+    ) : (
+      <LightRainSmall className="overflow-visible" />
+    );
   } else if (code === 4001 || code === 6001) {
-    icon = <Rain className="overflow-visible" />;
+    icon = big ? <Rain className="overflow-visible" /> : <RainSmall className="overflow-visible" />;
   } else if (code === 4201 || code === 6201) {
-    icon = <HeavyRain className="overflow-visible" />;
+    icon = big ? (
+      <HeavyRain className="overflow-visible" />
+    ) : (
+      <HeavyRainSmall className="overflow-visible" />
+    );
   } else if (code === 5001 || code === 5100 || code === 7102) {
-    icon = <LightSnow className="overflow-visible" />;
+    icon = big ? (
+      <LightSnow className="overflow-visible" />
+    ) : (
+      <LightSnowSmall className="overflow-visible" />
+    );
   } else if (code === 5000 || code === 7000) {
-    icon = <Snow className="overflow-visible" />;
+    icon = big ? <Snow className="overflow-visible" /> : <SnowSmall className="overflow-visible" />;
   } else if (code === 5101 || code === 7101) {
-    icon = <HeavySnow className="overflow-visible" />;
+    icon = big ? (
+      <HeavySnow className="overflow-visible" />
+    ) : (
+      <HeavySnowSmall className="overflow-visible" />
+    );
   } else if (code === 8000) {
     //TODO: fix thunder
-    icon = <Thunder className="overflow-visible" />;
+    icon = big ? (
+      <Thunder className="overflow-visible" />
+    ) : (
+      <ThunderSmall className="overflow-visible" />
+    );
   }
 
   return icon;
